88c63e94407dabb734867d7d04f77819bc09f8ae
khennyboy <abdullateefkehinde848@gmail.com>
2026-09-25 20:56:32 +0100

    first commit

--- src/components/FloatingTextArea.jsx
+++ src/components/FloatingTextArea.jsx
@@ -18,7 +18,7 @@ const FloatingTextarea = ({ label, error, ...props }) => {
           border="1px solid"
           borderColor={useColorModeValue("blackAlpha.100", "whiteAlpha.100")}
           rounded="xl"
-          h="150x"
+          h="150px"
           pt="24px"
           pb="6px"
           resize="none"
