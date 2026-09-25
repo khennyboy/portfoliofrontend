16ecc448c236dd9634468fa5db4c18db069c4d4e
khennyboy <abdullateefkehinde848@gmail.com>
2026-09-25 20:43:05 +0100

    first commit

--- src/components/ProjectsContent.jsx
+++ src/components/ProjectsContent.jsx
@@ -1,6 +1,6 @@
 import { SimpleGrid } from "@chakra-ui/react";
 import useGetProjects from "../hooks/getProject";
-import { slideInLeft, slideInRight } from "../utils/animation";
+import { scaleUp } from "../utils/animation";
 import { AnimatedSection } from "./AnimatedSection";
 import ProjectCard from "./ProjectCard";
 
@@ -10,9 +10,8 @@ const ProjectsContent = () => {
   return (
     <SimpleGrid columns={{ base: 1, md: 2, lg: 3 }} gap={{ base: 6, md: 8 }}>
       {data.map((p, index) => {
-        const variantType = index % 2 ? slideInLeft : slideInRight
         return (
-          <AnimatedSection variants={variantType}>
+          <AnimatedSection variants={scaleUp}>
             <ProjectCard key={p.title} project={p} />
           </AnimatedSection>
         );
